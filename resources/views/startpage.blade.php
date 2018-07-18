2ef8e4dd840e06bc7f04e2e4422a353dde7195a9
Jorge Serrano Junior <jorgeserarnojunior@hotmail.com>
2018-07-18 16:08:07 -0300

    Posts

--- resources/views/startpage.blade.php
+++ resources/views/startpage.blade.php
@@ -1,73 +1,183 @@
 @extends('layouts.header')
 @section('content')
-<h3 class="text-center">Adriano Dias dos Santos</h3>
-<div class="container-fluid">
-   <div class="row">
-      <div class="col-md-6 col-xs-12">
-         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-               <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-               <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-               <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-               <div class="carousel-item active">
-                  <img class="d-block w-100" src="/img/foto1.jpg" alt="First slide">
-               </div>
-               <div class="carousel-item">
-                  <img class="d-block w-100" src="/img/foto2.jpg" alt="Second slide">
-               </div>
-               <div class="carousel-item">
-                  <img class="d-block w-100" src="/img/foto3.jpg" alt="Third slide">
-               </div>
+
+
+<link rel="stylesheet" type="text/css" href="http://storage.googleapis.com/themevessel-items/real-house/css/style.css">
+<link rel="stylesheet" type="text/css" id="style_sheet" href="http://storage.googleapis.com/themevessel-items/real-house/css/skins/default.css">
+<link rel="stylesheet" type="text/css" href="http://storage.googleapis.com/themevessel-items/real-house/fonts/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<div class="properties-details-page content-area-6">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-12">
+                    <div id="propertiesDetailsSlider" class="carousel properties-details-sliders slide mb-40">
+                        <div class="heading-properties">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="pull-left">
+                                        <h3>Beautiful Single Home</h3>
+                                        <p><i class="fa fa-map-marker"></i> 20/F Green Road, Dhanmondi, Dhaka</p>
+                                    </div>
+                                    <div class="p-r">
+                                        <h3>$420,00</h3>
+                                        <p><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- main slider carousel items -->
+                        <div class="carousel-inner">
+                            <div class="item carousel-item" data-slide-number="0">
+                                <img src="/img/foto1.jpg" class="img-fluid" alt="slider-properties">
+                            </div>
+                            <div class="item carousel-item" data-slide-number="1">
+                                <img src="/img/foto2.jpg" class="img-fluid" alt="slider-properties">
+                            </div>
+                            <div class="item carousel-item" data-slide-number="2">
+                                <img src="/img/foto3.jpg" class="img-fluid" alt="slider-properties">
+                            </div>
+                            <div class="item carousel-item" data-slide-number="4">
+                                <img src="/img/foto2.jpg" class="img-fluid" alt="slider-properties">
+                            </div>
+                            <div class="item carousel-item active" data-slide-number="5">
+                                <img src="/img/foto1.jpg" class="img-fluid" alt="slider-properties">
+                            </div>
+    
+                            <a class="carousel-control left" href="#propertiesDetailsSlider" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                            <a class="carousel-control right" href="#propertiesDetailsSlider" data-slide="next"><i class="fa fa-angle-right"></i></a>
+    
+                        </div>
+                        <!-- main slider carousel nav controls -->
+                        </ul>
+                        <!-- main slider carousel items -->
+    
+                        <!-- Heading properties start -->
+                        <div class="heading-properties">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="pull-left">
+                                        <h3>Adriano Dias dos Santos</h3>
+                                        <p><i class="fa fa-map-marker"></i> Rua Adriano numero 123</p>
+                                    </div>
+                                    <div class="pull-right">
+                                        <h3><span class="text-right">R$235.000,00</span></h3>
+                                        <h5>VENDA</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Heading properties end -->
+                    </div>
+    
+                    
+                    <!-- Property meta end -->
+    
+    
+                    <!-- Properties description start -->
+                    <div class="properties-description mb-40">
+                        <h3 class="heading-2">
+                            Descrição
+                        </h3>
+                        <p>
+                        Av. Adriano Dias dos Santos, 313 Jardim Boa Esperanca, 
+CEP: 11470-220, Vicente de Carvalho - Sp
+Casa 3 - R$235.000.00
+Características do imóvel: Sobrado germinado sobreposto superior, 2 quartos e sacada gourmet - 61.295m²
+                        </p>
+                    </div>
+                    <!-- Properties description end -->
+    
+                  
+    
+                  
+    
+                    <!-- Location start -->
+                    <div class="location mb-50">
+                       <div class="map">
+                          <h3 class="heading-2">Localização</h3>
+                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3645.9921026322068!2d-46.278870865117476!3d-23.96071940455697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce01838b1e7645%3A0x128346b71ebd7bb!2sAv.+Adriano+Dias+dos+Santos+-+Jardim+Boa+Esperanca+(Vicente+de+Carvalho)%2C+Guaruj%C3%A1+-+SP!5e0!3m2!1spt-BR!2sbr!4v1486052507715" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+
+                          
+                       </div>
+                    </div>
+                    <!-- Location end -->
+    
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="sidebar-left">
+                        <!-- Search contents sidebar start -->
+                        <div class="widget search-area d-none d-xl-block d-lg-block">                            
+                            
+     <div class="fb-page" 
+                            data-href="https://www.facebook.com/lsstarincorporadora/"
+                            data-width="380" 
+                            data-hide-cover="false"
+                            data-show-facepile="false">
+                        </div>
+
+                        
+    
+                        <!-- Social media box start -->
+                            
+
+
+                        <div class="social-media-box widget clearfix">
+                            <h3 class="sidebar-title">Midias Sociais</h3>
+                            
+                            <ul class="social-list">
+                                <li>
+                                    <a href="#" class="facebook-bg">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="twitter-bg">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="google-bg">
+                                        <i class="fa fa-google"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="linkedin-bg">
+                                        <i class="fa fa-linkedin"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="pinterest-bg">
+                                        <i class="fa fa-pinterest"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+    
+                        <!-- Team 5 start -->
+                        <div class="team-5 widget">
+                            <h3 class="sidebar-title">Contatos</h3>
+                            <div class="media">
+                                <a href="agent-detail.html">
+                                    
+                                </a>
+                                <div class="media-body align-self-center">
+                                    <h5>
+                                        <a href="properties-details.html">Ana Paula</a>
+                                    </h5>
+                                    <p><i class="flaticon-technology-1"></i><a href="agent-detail.html">(11)99971-2869</a></p>
+                                    <p><i class="flaticon-envelope"></i><a href="tel:info@themevessel.com">lsstar@lsstar.com.br</a></p>
+                                </div>
+                            </div>                            
+                        </div>
+    
+                        
+    
+                    </div>
+                </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-            </a>
-         </div>
-      </div>
-      <div class="col-md-6 col-xs-12 mapa">
-         <div>
-            <ul class="nav nav-tabs">
-                 
-                <li role="presentation" class="nav-item">
-                    <a href="#home"class="nav-link active" aria-controls="home" role="tab" data-toggle="tab" aria-expanded="true">
-                        Descrição
-                    </a>
-                </li>
-                <li role="presentation" class="nav-item">
-                    <a href="#profile"class="nav-link" aria-controls="home" role="tab" data-toggle="tab" aria-expanded="true">
-                        Mapa
-                    </a>
-                </li>
-                           
-            </ul> 
             
-            <!-- Tab panes -->
-            <div class="tab-content">
-               <div role="tabpanel" class="tab-pane active" id="home">
-                  <span class="title-casa">Casas à venda</span> - Projeto, Arq. <strong> Flavia L. L. Santos.</strong><br><br>
-                  Av. Adriano Dias dos Santos, 313
-                  Jardim Boa Esperanca, <br> CEP: 11470-220,
-                  Vicente de Carvalho - Sp
-                  <h4>Casa 3 - <span class="text-primary">R$235.000.00</span></h4>
-                  Características do imóvel:
-                  Sobrado germinado sobreposto superior, 2 quartos e sacada gourmet - 61.295m²<br>
-                   </div>
-               <div role="tabpanel" class="tab-pane" id="profile">
-                  <div class="maps">
-                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3645.9921026322068!2d-46.278870865117476!3d-23.96071940455697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce01838b1e7645%3A0x128346b71ebd7bb!2sAv.+Adriano+Dias+dos+Santos+-+Jardim+Boa+Esperanca+(Vicente+de+Carvalho)%2C+Guaruj%C3%A1+-+SP!5e0!3m2!1spt-BR!2sbr!4v1486052507715" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
-                  </div>
-               </div>
-            </div>
-         </div>
-              
-      </div>
-   </div>
-</div>
+        </div>
+    </div>
 @endsection
